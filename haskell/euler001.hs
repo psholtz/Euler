3b7f3e1c5c8c1514d58bf77e077681cf6f6cdd5f
psholtz <paul.sholtz@gmail.com>
2011-09-29 09:14:54 -0700

    updating answer

--- haskell/euler001.hs
+++ haskell/euler001.hs
@@ -5,7 +5,7 @@
 -- Find the sum of all the multiples of 3 or 5 below 1000.
 --
 
--- The problem is easily tackled using Haskell set comprehensions:
+-- The problem is easily tackled using Haskell list comprehensions:
 
 euler001 = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
 
